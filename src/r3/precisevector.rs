4a01c1400e593fed177d763252b0e8e8bef68ad9
Alexander Haynes <alexander@eltoro.com>
2019-06-17 15:37:19 -0400

    all tests but one passing

--- src/r3/precisevector.rs
+++ src/r3/precisevector.rs
@@ -79,7 +79,7 @@ impl PreciseVector {
 
 	// Norm2 returns the square of the norm.
 	pub fn norm2(&self) -> bigdecimal::BigDecimal {
-		return self.dot(&self.clone());
+		return self.dot(self.clone());
 	}
 
 	// IsUnit reports whether this vector is of unit length.
@@ -132,7 +132,7 @@ impl PreciseVector {
 	}
 
 	// Dot returns the standard dot product of v and ov.
-	pub fn dot(&self, ov: &PreciseVector) -> bigdecimal::BigDecimal{
+	pub fn dot(&self, ov: PreciseVector) -> bigdecimal::BigDecimal{
 		let a = self.clone();
 		let b = ov.clone();
 		return (a.x * b.x) + (a.y * b.y) + (a.z * b.z)
@@ -183,5 +183,209 @@ impl PreciseVector {
         }
 	}
 
+}
+
+#[cfg(test)]
+mod tests {
+
+	use r3::vector::Axis;
+	use r3::precisevector;
+	use r3::vector::Vector;
+	use consts::EPSILON;
+	use r3::precisevector::bigdecimal::ToPrimitive;
+
+	#[test]
+	pub fn test_precise_round_trip() {
+
+		let tv = Vector{x:1.0, y:2.0, z:3.0};
+		println!("tv.x: {}", tv.x);
+		let tvnorm = tv.clone().normalize();
+		println!("tvnorm.x: {}", tvnorm.x);
+		let ptv = precisevector::precise_vector_from_vector(tv.clone());
+		println!("ptv.x: {}", ptv.x);
+		let ptvret = ptv.clone().vector();
+		println!("ptvret.x: {}", ptvret.x);
+		println!("----------------------------------------");
+		let nptv = ptv.clone();
+		let x = nptv.x.to_f64().unwrap();
+		let y = nptv.y.to_f64().unwrap();
+		let z = nptv.z.to_f64().unwrap();
+		let manual = Vector{x, y, z}.normalize();
+		println!("tv.x: {}    manual.x: {}", tv.x, nptv.x);
+
+		assert_eq!(tvnorm.x, manual.x);
+
+		assert!(precisevector::precise_vector_from_vector(Vector{x:0.0, y:0.0, z:0.0}).vector() == Vector{x:0.0, y:0.0, z:0.0}.normalize());
+		assert!(precisevector::precise_vector_from_vector(Vector{x:1.0, y:2.0, z:3.0}).vector() == Vector{x:1.0, y:2.0, z:3.0}.normalize());
+		assert!(precisevector::precise_vector_from_vector(Vector{x:3.0, y:-4.0, z:12.0}).vector() == Vector{x:3.0, y:-4.0, z:12.0});
+		assert!(precisevector::precise_vector_from_vector(Vector{x:1.0, y:1e-16_f64, z:1e-32_f64}).vector() == Vector{x:1.0, y:1e-16_f64, z:1e-32_f64});
+	}
+
+	#[test]
+	pub fn test_precise_is_unit() {
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 0.0).is_unit() == false);
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).is_unit() == true);
+		assert!(precisevector::new_precise_vector(0.0, 1.0, 0.0).is_unit() == true);
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 1.0).is_unit() == true);
+		assert!(precisevector::new_precise_vector(1.0+2.0*EPSILON, 0.0, 0.0).is_unit() == false);
+		assert!(precisevector::new_precise_vector(0.0 * (1.0+EPSILON), 0.0, 0.0).is_unit() == false);
+		assert!(precisevector::new_precise_vector(1.0, 1.0, 1.0).is_unit() == false);
+	}
+
+	#[test]
+	pub fn test_precise_norm2() {
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 0.0).norm2() == precisevector::prec_float(0.0));
+		assert!(precisevector::new_precise_vector(0.0, 1.0, 0.0).norm2() == precisevector::prec_float(1.0));
+		assert!(precisevector::new_precise_vector(1.0, 1.0, 1.0).norm2() == precisevector::prec_float(3.0));
+		assert!(precisevector::new_precise_vector(1.0, 2.0, 3.0).norm2() == precisevector::prec_float(14.0));
+		assert!(precisevector::new_precise_vector(3.0, -4.0, 12.0).norm2() == precisevector::prec_float(169.0));
+	}
+
+	#[test]
+	pub fn test_precise_add() {
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 0.0).add(precisevector::new_precise_vector(0.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)) == true);
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).add(precisevector::new_precise_vector(0.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(1.0, 0.0, 0.0)) == true);
+		assert!(precisevector::new_precise_vector(1.0, 2.0, 3.0).add(precisevector::new_precise_vector(4.0, 5.0, 7.0)).equal(precisevector::new_precise_vector(5.0, 7.0, 10.0)) == true);
+		assert!(precisevector::new_precise_vector(1.0, -3.0, 5.0).add(precisevector::new_precise_vector(1.0, -6.0, -6.0)).equal(precisevector::new_precise_vector(2.0, -9.0, -1.0)) == true);
+	}
+
+	#[test]
+	pub fn test_precise_sub() {
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 0.0).sub(precisevector::new_precise_vector(0.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)) == true);
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).sub(precisevector::new_precise_vector(0.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(1.0, 0.0, 0.0)) == true);
+		assert!(precisevector::new_precise_vector(1.0, 2.0, 3.0).sub(precisevector::new_precise_vector(4.0, 5.0, 7.0)).equal(precisevector::new_precise_vector(-3.0, -3.0, -4.0)) == true);
+		assert!(precisevector::new_precise_vector(1.0, -3.0, 5.0).sub(precisevector::new_precise_vector(1.0, -6.0, -6.0)).equal(precisevector::new_precise_vector(0.0, 3.0, 11.0)) == true);
+	}
+
+	#[test]
+	pub fn test_precise_mul() {
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 0.0).mul(precisevector::prec_float(3.0)).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).mul(precisevector::prec_float(1.0)).equal(precisevector::new_precise_vector(1.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).mul(precisevector::prec_float(0.0)).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).mul(precisevector::prec_float(3.0)).equal(precisevector::new_precise_vector(3.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, -3.0, 5.0).mul(precisevector::prec_float(-1.0)).equal(precisevector::new_precise_vector(-1.0, 3.0, -5.0)));
+		assert!(precisevector::new_precise_vector(1.0, -3.0, 5.0).mul(precisevector::prec_float(2.0)).equal(precisevector::new_precise_vector(2.0, -6.0, 10.0)));
+	}
+
+	#[test]
+	pub fn test_precise_mul_by_f64() {
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 0.0).mul_by_f64(3.0).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).mul_by_f64(1.0).equal(precisevector::new_precise_vector(1.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).mul_by_f64(0.0).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).mul_by_f64(3.0).equal(precisevector::new_precise_vector(3.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, -3.0, 5.0).mul_by_f64(-1.0).equal(precisevector::new_precise_vector(-1.0, 3.0, -5.0)));
+		assert!(precisevector::new_precise_vector(1.0, -3.0, 5.0).mul_by_f64(2.0).equal(precisevector::new_precise_vector(2.0, -6.0, 10.0)));
+	}
+
+	#[test]
+	pub fn test_precise_dot() {
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).dot(precisevector::new_precise_vector(1.0, 0.0, 0.0)) == precisevector::prec_float(1.0));
+		assert!(precisevector::new_precise_vector(0.0, 1.0, 0.0).dot(precisevector::new_precise_vector(0.0, 1.0, 0.0)) == precisevector::prec_float(1.0));
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 1.0).dot(precisevector::new_precise_vector(0.0, 0.0, 1.0)) == precisevector::prec_float(1.0));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).dot(precisevector::new_precise_vector(0.0, 1.0, 0.0)) == precisevector::prec_float(0.0));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).dot(precisevector::new_precise_vector(0.0, 1.0, 1.0)) == precisevector::prec_float(0.0));
+		assert!(precisevector::new_precise_vector(1.0, 1.0, 1.0).dot(precisevector::new_precise_vector(-1.0, -1.0, -1.0)) == precisevector::prec_float(-3.0));
+	}
+
+	#[test]
+	pub fn test_precise_cross() {
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).cross(precisevector::new_precise_vector(1.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(0.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(1.0, 0.0, 0.0).cross(precisevector::new_precise_vector(0.0, 1.0, 0.0)).equal(precisevector::new_precise_vector(0.0, 0.0, 1.0)));
+		assert!(precisevector::new_precise_vector(0.0, 1.0, 0.0).cross(precisevector::new_precise_vector(0.0, 0.0, 1.0)).equal(precisevector::new_precise_vector(1.0, 0.0, 0.0)));
+		assert!(precisevector::new_precise_vector(0.0, 0.0, 1.0).cross(precisevector::new_precise_vector(1.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(0.0, 1.0, 0.0)));
+		assert!(precisevector::new_precise_vector(0.0, 1.0, 0.0).cross(precisevector::new_precise_vector(1.0, 0.0, 0.0)).equal(precisevector::new_precise_vector(0.0, 0.0, -1.0)));
+		assert!(precisevector::new_precise_vector(1.0, 2.0, 3.0).cross(precisevector::new_precise_vector(-4.0, 5.0, -6.0)).equal(precisevector::new_precise_vector(-27.0, -6.0, 13.0)));
+	}
+
+	#[test]
+	pub fn test_precise_identities() {
+		let v1 = precisevector::new_precise_vector(0.0, 0.0, 0.0);
+		let v2 = precisevector::new_precise_vector(0.0, 0.0, 0.0);
+		let c1 = v1.cross(v2.clone());
+		let c2 = v2.cross(v1.clone());
+		let d1 = v1.dot(v2.clone());
+		let d2 = v2.dot(v1.clone());
+		assert!(d1 == d2);
+		assert!(c1.equal(c2.mul_by_f64(-1.0)) == true);
+		assert!(v1.dot(c1.clone()) == precisevector::prec_float(0.0));
+		assert!(v2.dot(c1.clone()) == precisevector::prec_float(0.0));
+
+		let v1 = precisevector::new_precise_vector(0.0, 0.0, 0.0);
+		let v2 = precisevector::new_precise_vector(0.0, 1.0, 2.0);
+		let c1 = v1.cross(v2.clone());
+		let c2 = v2.cross(v1.clone());
+		let d1 = v1.dot(v2.clone());
+		let d2 = v2.dot(v1.clone());
+		assert!(d1 == d2);
+		assert!(c1.equal(c2.mul_by_f64(-1.0)) == true);
+		assert!(v1.dot(c1.clone()) == precisevector::prec_float(0.0));
+		assert!(v2.dot(c1.clone()) == precisevector::prec_float(0.0));
+
+		let v1 = precisevector::new_precise_vector(1.0, 0.0, 0.0);
+		let v2 = precisevector::new_precise_vector(0.0, 1.0, 0.0);
+		let c1 = v1.cross(v2.clone());
+		let c2 = v2.cross(v1.clone());
+		let d1 = v1.dot(v2.clone());
+		let d2 = v2.dot(v1.clone());
+		assert!(d1 == d2);
+		assert!(c1.equal(c2.mul_by_f64(-1.0)) == true);
+		assert!(v1.dot(c1.clone()) == precisevector::prec_float(0.0));
+		assert!(v2.dot(c1.clone()) == precisevector::prec_float(0.0));
+
+		let v1 = precisevector::new_precise_vector(1.0, 0.0, 0.0);
+		let v2 = precisevector::new_precise_vector(0.0, 1.0, 1.0);
+		let c1 = v1.cross(v2.clone());
+		let c2 = v2.cross(v1.clone());
+		let d1 = v1.dot(v2.clone());
+		let d2 = v2.dot(v1.clone());
+		assert!(d1 == d2);
+		assert!(c1.equal(c2.mul_by_f64(-1.0)) == true);
+		assert!(v1.dot(c1.clone()) == precisevector::prec_float(0.0));
+		assert!(v2.dot(c1.clone()) == precisevector::prec_float(0.0));
+
+		let v1 = precisevector::new_precise_vector(1.0, 1.0, 1.0);
+		let v2 = precisevector::new_precise_vector(-1.0, -1.0, -1.0);
+		let c1 = v1.cross(v2.clone());
+		let c2 = v2.cross(v1.clone());
+		let d1 = v1.dot(v2.clone());
+		let d2 = v2.dot(v1.clone());
+		assert!(d1 == d2);
+		assert!(c1.equal(c2.mul_by_f64(-1.0)) == true);
+		assert!(v1.dot(c1.clone()) == precisevector::prec_float(0.0));
+		assert!(v2.dot(c1.clone()) == precisevector::prec_float(0.0));
+
+		let v1 = precisevector::new_precise_vector(1.0, 2.0, 2.0);
+		let v2 = precisevector::new_precise_vector(-0.3, 0.4, -1.2);
+		let c1 = v1.cross(v2.clone());
+		let c2 = v2.cross(v1.clone());
+		let d1 = v1.dot(v2.clone());
+		let d2 = v2.dot(v1.clone());
+		assert!(d1 == d2);
+		assert!(c1.equal(c2.mul_by_f64(-1.0)) == true);
+		assert!(v1.dot(c1.clone()) == precisevector::prec_float(0.0));
+		assert!(v2.dot(c1.clone()) == precisevector::prec_float(0.0));
+	}
+
+	#[test]
+	pub fn test_precise_largest_smallest_components() {
+		let v1 = precisevector::new_precise_vector(0.0, 0.0, 0.0);
+		assert!(v1.largest_component() == Axis::Z);
+		assert!(v1.smallest_component() == Axis::Z);
+		let v1 = precisevector::new_precise_vector(1.0, 0.0, 0.0);
+		assert!(v1.largest_component() == Axis::X);
+		assert!(v1.smallest_component() == Axis::Z);
+		let v1 = precisevector::new_precise_vector(1.0, -1.0, 0.0);
+		assert!(v1.largest_component() == Axis::Y);
+		assert!(v1.smallest_component() == Axis::Z);
+		let v1 = precisevector::new_precise_vector(-1.0, -1.1, -1.1);
+		assert!(v1.largest_component() == Axis::Z);
+		assert!(v1.smallest_component() == Axis::X);
+		let v1 = precisevector::new_precise_vector(0.5, -0.4, -0.5);
+		assert!(v1.largest_component() == Axis::Z);
+		assert!(v1.smallest_component() == Axis::Y);
+		let v1 = precisevector::new_precise_vector(1e-15, 1e-14, 1e-13);
+		assert!(v1.largest_component() == Axis::Z);
+		assert!(v1.smallest_component() == Axis::X);
+	} 
 }
 
